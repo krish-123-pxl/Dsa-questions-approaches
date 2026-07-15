d8c9d6cf1a36748242037dac226b3bfd5cf7967c
Krishna <ram620155@gmail.com>
2026-07-15 19:34:53 +0530

    adding additional approach to solve the pallindrome problem via reversing the LL from middle element to the last node.

--- linkedlist/checkPallindromeOfLL.js
+++ linkedlist/checkPallindromeOfLL.js
@@ -30,3 +30,74 @@ const checkPallindromeThroughArray = head => {
     return 1
 }
 
+// But here the space complexity is O(n) because we are using an extra array to solve the question.
+// We can solve this with an another method.
+// 1 -> 2 -> 3 -> 3 -> 2 -> 1
+// In this appraoch firstly we find the middle of the LL and then reverse the LL from next of the middle to the end of the ll.
+// And then compares the corresponding element and where the values don't match there we can return 0/false.
+
+const checkPallindromeThroughReverse = head => {
+    if(!head) return null;
+
+    let temp = head;
+    const middle = getMid(head);
+
+    // Now reverse the LL from next element of middle to end of the LL.
+    let prevRef = null;
+    let current = middle.next;
+    let forwardRef = current.next;
+
+    while(current) {
+        forwardRef = current.next;
+        current.next = prevRef;
+        prevRef = current;
+        current = forwardRef;
+    }
+    // after reverse the next of the middle points to the null.
+    // so conect the middle.next to prevRef element.
+    middle.next = prevRef;
+
+    // after reversing comparing the corresponding element.
+    let first = head;
+    let second = middle.next; // start from the first element of reverse sorted.
+    while(first && second) {
+        if(first.data !== second.data) return false;
+        
+        // Now update the both pointers.
+        first = first.next;
+        second = second.next;
+    }
+
+    // Now reverse back to obtain the original LL.
+    prevRef = null;
+    current = middle.next;
+    forwardRef = current.next;
+
+    while(current) {
+        forwardRef = current.next;
+        current.next = prevRef;
+        prevRef = current;
+        current = forwardRef;
+    }
+    middle.next = prevRef;
+    return true;
+}
+
+// here we find the middle of the LL.
+const getMid = head => {
+    // we use two pointers appraoch one is slowPointer and second is fastPointer.
+    // When the fastPointer traverse the whole LL then then slowPointer will be present at the middle of LL.
+    let slowPointer = head;
+    let fastPointer = head.next;
+
+    while(fastPointer) {
+        slowPointer = slowPointer.next;
+        fastPointer = fastPointer.next;
+
+        if(fastPointer) {
+            fastPointer = fastPointer.next;
+        }
+    }
+
+    return slowPointer;
+}
